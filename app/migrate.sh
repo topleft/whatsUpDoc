a96429cf277d2f459184ff3888f018db70fe7824
Pete Jeffryes <pete@flowhub.co>
2017-05-07 19:16:06 -0600

    migrations and tests working

--- app/migrate.sh
+++ app/migrate.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker-compose run user-service knex migrate:latest --env development --knexfile ./knexfile.js
+docker-compose run user-service knex migrate:latest --env development --knexfile app/knexfile.js
+docker-compose run user-service knex migrate:latest --env test --knexfile app/knexfile.js

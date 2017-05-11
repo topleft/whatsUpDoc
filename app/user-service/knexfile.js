bfbd89d01d1d731b55e0b44f9ab4cfd2b7b21127
Pete Jeffryes <pete@flowhub.co>
2017-05-11 07:54:19 -0600

    cleaned pu knexfiles in services

--- app/user-service/knexfile.js
+++ app/user-service/knexfile.js
@@ -4,21 +4,9 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: process.env.WUD_DATABASE_URL_DEV,
-    migrations: {
-      directory: path.join(__dirname, '/src', '/migrations')
-    },
-    seeds: {
-      directory: path.join(__dirname, '/src', '/seeds')
-    }
   },
   test: {
     client: 'postgresql',
     connection: process.env.WUD_DATABASE_URL_TEST,
-    migrations: {
-      directory: path.join(__dirname, '/src', '/migrations')
-    },
-    seeds: {
-      directory: path.join(__dirname, '/src', '/seeds')
-    }
   }
 };
